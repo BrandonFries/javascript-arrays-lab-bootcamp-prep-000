24f2cb470488582de8027bcd72b6e35d38e46485
Brandon Fries <brandonfries1993@gmail.com>
2019-06-06 15:05:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]
+var moreKittens = ["Broom"]
+var evenMoreKittens = ["Arnold"]
 
-// Add your functions and code here
+function destructivelyAppendKitten(Ralph) {
+  return kittens.push(Ralph)
+}
+function destructivelyPrependKitten(Bob) {
+  return kittens.unshift(Bob)
+}
+ function destructivelyRemoveLastKitten(Garfield) {
+   return kittens.pop(Garfield)
+ }
+function destructivelyRemoveFirstKitten(Milo) {
+  return kittens.shift(Milo)
+}
+function appendKitten(Broom) {
+  return moreKittens = kittens.concat("Broom");
+
+  console.log(moreKittens);
+}
+function prependKitten(Arnold) {
+  var theCats = evenMoreKittens.concat(kittens);
+  return theCats
+}
+function removeLastKitten(Garfield) {
+  console.log(kittens.slice(3))
+}
